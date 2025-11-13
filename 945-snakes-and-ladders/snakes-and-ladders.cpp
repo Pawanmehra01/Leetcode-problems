14fca20adac3949e46e29ca58f7ae791c96cd2ff
Pawan singh mehra <Pawan7mehra12@gmail.com>
2025-11-13 21:18:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (77.56%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int n ;
+    pair<int,int>getCord(int num ){
+           int rowfromtop = (num-1)/n ;
+           int rowfrombottom = (n-1)-rowfromtop ;
+
+           int col = (num-1)%n;
+
+           if((n%2 == 1 && rowfrombottom%2 ==1)||(n%2 == 0 && rowfrombottom %2 == 0)){
+            col = (n-1)-col ;
+           }
+           return make_pair(rowfrombottom,col);
+
+    }
+    int snakesAndLadders(vector<vector<int>>& board) {
+        n = board.size() ;
+        int steps = 0 ;
+        queue<int>q ;
+        vector<vector<bool>>visited(n,vector<bool>(n,false)) ;
+        visited[n-1][0] = true ;
+        q.push(1) ;
+
+        while(!q.empty()){
+            int N = q.size();
+
+            while(N--){
+                int x = q.front() ;
+                q.pop();
+                
+                if(x == n*n)return steps ;
+
+                for(int k=1 ;k<=6;k++){
+
+                    int val = x+k ;
+
+                    if(val > n*n){
+                        break ;
+                    }
+                    pair<int,int>coord = getCord(val);
+                    int r = coord.first ;
+                    int c = coord.second ;
+
+                    if(visited[r][c] ==  true)continue ;
+
+                    visited[r][c] = true;
+
+                    if(board[r][c] == -1){
+                        q.push(val);
+                    }else{
+                        q.push(board[r][c]);
+                    }
+                }
+            }steps++ ;
+        }
+        return -1 ;
+    }
+};
