82ce7249abebad23931440e80859519c838e02d0
Pawan singh mehra <Pawan7mehra12@gmail.com>
2025-11-20 13:32:36 +0530

    Time: 51 ms (94.72%) | Memory: 173 MB (88.63%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,70 +1,74 @@
 class LRUCache {
-    private:
     struct Node{
-        int key , value ;
-        Node* prev;
-        Node* next;
-        Node(int k , int v){
-            key = k;
-            value = v ;
-            prev = nullptr;
-            next = nullptr;
-    }
-};
+        int key  ;
+        int value ; 
+        Node* next ;
+        Node* prev ;
+        Node(int key , int value ){
+            this-> key = key ;
+            this-> value = value ;
+            next = NULL ;
+            prev = NULL ;
+        }
+
+    };
 public:
-    int capacity_;
-    map<int , Node*>mpp;
-    Node* head;
-    Node* tail;
-LRUCache(int capacity) {
-        capacity_=capacity;
-       
-        head = new Node(-1,-1);
-        tail = new Node(-1,-1);
-        head->next = tail;
-        tail->prev = head;  
-}
-void deletenode(Node* node){
-       Node * prevNode=node->prev;
-       Node* afterNode=node->next;
-       prevNode->next=afterNode;
-       afterNode->prev=prevNode;
-    }
-void insertAfterHead(Node* node){
-        Node * curr = head -> next;
-        head->next=node;
-        node->prev=head;
-        curr->prev=node;
-        node->next=curr;
-    }
+    int capacity ;
+    unordered_map<int,Node*>mp; 
+    Node* head ;
+    Node* tail  ;
     
-int  get(int key) {
-        if(mpp.find(key)==mpp.end())return -1;
-        Node* node = mpp[key];
-        deletenode(node);
-        insertAfterHead(node);
-        return node->value;        
+    LRUCache(int capacity) {
+        this->capacity = capacity ;
+        mp.clear(); 
+        tail = new Node(-1,-1) ;
+        head = new Node (-1,-1) ;
+        head ->next = tail ;
+        tail ->prev = head  ;
+
     }
+    void deletenode(Node* node){
+        Node* prevNode  = node->prev ;
+        Node* afternode = node ->next ;
+        prevNode->next = afternode ;
+        afternode ->prev = prevNode ;
+    }
+     void insertafterhead(Node* node ){
+        Node* curr =  head ->next ;
+        head->next = node ;
+        node->prev = head ;
+        curr->prev = node ;
+        node->next = curr ;
+    }
+    int get(int key) {
+        if(mp.find(key)==mp.end())return -1 ;
+        Node * node = mp[key] ; 
+        deletenode(node) ;
+        insertafterhead(node);
+        return node -> value ;
+        }
+        
+    
     
     void put(int key, int value) {
-        if(mpp.find(key)!=mpp.end()){
-            Node* node = mpp[key];
+        if(mp.find(key)!=mp.end()){
+            Node* node = mp[key] ;
             node->value = value ;
             deletenode(node);
-            insertAfterHead(node);
-        }else{
-            if(mpp.size()==capacity_){
-                Node* node = tail->prev;
-                mpp.erase(node->key);
-                deletenode(node);
-                delete node;
+            insertafterhead(node);
             }
-            Node* node =new Node(key,value);
-            mpp[key]=node;
-            insertAfterHead(node);
-        } 
-      }
-    
+            else{
+            if(mp.size()==capacity){
+                Node* node = tail->prev ;
+                mp.erase(node -> key) ;
+                deletenode(node) ;
+            }
+            Node* nde = new Node(key,value);
+            mp[key] = nde ;
+            insertafterhead(nde);
+
+        }
+    }
 };
 
 /**
