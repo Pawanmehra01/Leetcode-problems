b6438f9ad290bf72d48269a1479ef8fdc44589f8
Pawan singh mehra <Pawan7mehra12@gmail.com>
2025-11-25 16:59:59 +0530

    Time: 55 ms (8.55%) | Memory: 11 MB (97.51%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[501][501];
+    int solve(string& word1 ,string& word2,int n ,int m ){
+        if(n == 0 || m == 0 )return n+m;
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(word1[n-1] ==  word2[m-1])return solve(word1,word2,n-1,m-1);
+
+        return dp[n][m] = 1 + min(solve(word1,word2,n,m-1),solve(word1,word2,n-1,m));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(word1,word2,n,m) ;
+    }
+};
