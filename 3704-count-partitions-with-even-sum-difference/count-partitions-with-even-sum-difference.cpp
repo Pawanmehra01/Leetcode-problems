4fd30f1146c87e3634d1e2bc76c182840d26c78b
Pawan singh mehra <Pawan7mehra12@gmail.com>
2025-12-05 12:22:13 +0530

    Time: 39 ms (3.41%) | Memory: 23.2 MB (5.30%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -4,20 +4,22 @@ public:
         int n = nums.size();
         vector<int>prefix(n);
         int sum = 0 ;
-        int count =0 ;
+        int count = 0;
         for( int i = 0 ;i < n ; i ++ ){
             prefix[i] = sum +nums[i] ;
             sum = prefix[i] ;
         }
-        if(prefix[n-1]%2 != 0)return  0 ;
+       
         int ans  = 0;
         int i =0 ;
         while(i<n){
             int ans = prefix[i] ;
-            int sol = abs(prefix[n-1] - prefix[i]) ;
-            if((ans+sol)%2==0 )count++ ;
+            int sol = prefix[n-1] - prefix[i];
+            if(abs((ans-sol))%2==0 )count++ ;
+            cout << "sum diff"<<ans-sol<<endl;
+            cout<<"count"<<count<<endl;
             i++ ;
-        }
-        return count-1;
+        }if(count == 0)return 0 ;
+        return count -1 ;
     }
 };
