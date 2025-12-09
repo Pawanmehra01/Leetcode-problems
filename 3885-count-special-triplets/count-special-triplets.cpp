fa61585e0986919dfae6d625b2b7c2a959e997e8
Pawan singh mehra <Pawan7mehra12@gmail.com>
2025-12-09 18:05:47 +0530

    Time: 859 ms (57.30%) | Memory: 427.9 MB (44.52%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -5,13 +5,13 @@ int M = 1e9+7;
         unordered_map<int,int>xleft;
         unordered_map<int,int>xright;
         int n = nums.size();
-     
+        xleft[nums[0]]++ ;
         for( auto num:nums){
             xright[num]++ ;
         }
         int ans = 0 ;
-      
-        for(int i = 0 ;i< n;i ++){
+        xright[nums[0]]-- ;
+        for(int i = 1 ;i< n-1 ;i ++){
             xright[nums[i]]--;
             int left  = xleft[nums[i]*2];
             int right = xright[nums[i]*2];
