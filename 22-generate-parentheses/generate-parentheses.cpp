81f760fddcc716c679c243adac1d5bb2ae21d48b
Pawan singh mehra <Pawan7mehra12@gmail.com>
2025-12-19 08:50:58 +0530

    Time: 4 ms (34.12%) | Memory: 15.7 MB (49.84%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -8,6 +8,17 @@ public:
             solve(open-1 ,close,ans,add+"(");
         }
         if(close>open)solve(open ,close-1 ,ans,add+")");
+        // if (left > right || left < 0) return;
+        // if (left + right == 0) {
+        //     result.push_back(parens);
+        //     return;
+        // }
+        // // parens.push_back('(');
+        // collect(result, parens, left - 1, right);
+        // parens.pop_back();
+        // parens.push_back(')');
+        // collect(result, parens, left, right - 1);
+        // parens.pop_back();
     }
     vector<string> generateParenthesis(int n) {
         vector<string>ans ;
