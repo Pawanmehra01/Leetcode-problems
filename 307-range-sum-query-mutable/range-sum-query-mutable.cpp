810a9da1cd8183d771a54cb1f2137c7916779220
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-01-15 11:19:36 +0530

    Time: 51 ms (72.13%) | Memory: 182.2 MB (53.96%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,57 +1,59 @@
 class NumArray {
 public:
-    vector<int> Tree;
-    int n;
-
-    void buildSegTree(int node, int l, int r, vector<int>& nums) {
-        if (l == r) {
-            Tree[node] = nums[l];
-            return;
+    vector<int>Tree;
+    int n ;
+    void buildSegTree(int i, int l ,int r ,vector<int>&nums){
+        if(l==r){
+        Tree[i] = nums[r];
+        return ;
         }
-        int mid = (l + r) / 2;
-        buildSegTree(2 * node + 1, l, mid, nums);
-        buildSegTree(2 * node + 2, mid + 1, r, nums);
-        Tree[node] = Tree[2 * node + 1] + Tree[2 * node + 2];
+
+        int mid =(l+r)/2 ;
+        buildSegTree(2*i+1 ,l ,mid ,nums);
+        buildSegTree(2*i+2,mid+1 ,r,nums);
+        Tree[i] = Tree[2*i+1] + Tree[2*i+2];
     }
 
     NumArray(vector<int>& nums) {
         n = nums.size();
-        Tree.resize(4 * n);
-        buildSegTree(0, 0, n - 1, nums);
+        Tree.resize(4*n,0);
+        buildSegTree(0 ,0,n-1,nums);
     }
-
-    void updateSegTree(int index, int val, int l, int r, int node) {
-        if (l == r) {
-            Tree[node] = val;
-            return;
-        }
-        int mid = (l + r) / 2;
-        if (index <= mid)
-            updateSegTree(index, val, l, mid, 2 * node + 1);
-        else
-            updateSegTree(index, val, mid + 1, r, 2 * node + 2);
-
-        Tree[node] = Tree[2 * node + 1] + Tree[2 * node + 2];
+    
+    void updateSegTree(int index , int val , int l , int r, int node){
+    if(l==r){
+        Tree[node] = val;
+        return ;
+    }
+    int  mid = (l+r)/2 ;
+    if(index<=mid){
+       updateSegTree(index , val , l,mid,2*node+1);
+    }
+    else{
+       updateSegTree(index, val , mid+1 ,r ,2*node+2);
+    }
+    Tree[node] = Tree[2*node + 1] + Tree[2*node+2 ];
     }
 
     void update(int index, int val) {
-        updateSegTree(index, val, 0, n - 1, 0);
+        int  i = 0 ;
+        int  l = 0 ;
+        int  r = n-1;
+        updateSegTree(index,val,l,r,i);
     }
-
-    int querySum(int left, int right, int ql, int qr, int node) {
-        if (qr < left || ql > right)
-            return 0;
-
-        if (ql <= left && right <= qr)
+    int querySum(int left ,int right , int start ,int end,int node){
+        if(left>end || right<start)return 0 ;
+        int mid =(left+right)/2;
+        if(left>=start && right<=end ){
             return Tree[node];
-
-        int mid = (left + right) / 2;
-        return querySum(left, mid, ql, qr, 2 * node + 1) +
-               querySum(mid + 1, right, ql, qr, 2 * node + 2);
+        }
+        return querySum(left ,mid,start,end,2*node+1) + querySum(mid+1,right,start,end,   2*node+2);
     }
-
+    
     int sumRange(int left, int right) {
-        return querySum(0, n - 1, left, right, 0);
+        int start = 0 ;
+        int end = n-1 ;
+        return querySum(start,end ,left,right,0);
     }
 };
 
