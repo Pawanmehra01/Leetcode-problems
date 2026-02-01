a7ea65c65f9cd3984e85b44e2ab4c961e012f7c8
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-02-01 07:55:29 +0530

    Time: 4 ms (77.39%) | Memory: 21.3 MB (13.79%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int area(vector<int>&heights){
-    int n = heights.size();
+int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
         vector<int>nger(n);
         vector<int>ngel(n,-1);
         stack<int>s;
@@ -15,7 +15,7 @@ public:
             s.push(i);
         }
        
-        while(!s.empty())s.pop();
+       while(!s.empty())s.pop();
 
         for(int i = n-1 ;i>=0 ;i -- ){
             while(!s.empty() && heights[s.top()]>= heights[i]){
@@ -28,35 +28,27 @@ public:
             }
             s.push(i);
         }
-    
         int maxArea = 0 ;
         for(int i = 0 ;i < n ;i ++){
             int breadth  = nger[i]-ngel[i]-1 ;
             maxArea = max(maxArea , heights[i]*breadth );
 
         }
-        return maxArea ;
+    return maxArea;
     }
-
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        
         int n = matrix.size();
-        int m = matrix[0].size();
-        if(n==0)return 0 ;
-        vector<int>hist(m,0);
-        int ans  = 0 ;
-        for( int i =0 ;i < n ;i ++){
-            for( int j =0 ;j< m ;j++){
-               if(matrix[i][j]=='1')hist[j]++;
-            else{
-                hist[j] = 0;
+        int m =matrix[0].size();
+        int MaxArea = INT_MIN;
+        vector<int>heights(m,0);
+        for(int i =0 ;i<n;i++){
+            for(int j =0 ;j< m;j ++){
+                if(matrix[i][j]=='1')heights[j]++;
+                if(matrix[i][j]=='0')heights[j] = 0;
             }
-            
+            int Area = largestRectangleArea(heights);
+            MaxArea = max(Area,MaxArea);
         }
-        ans= max(ans,area(hist));
-        
-        }
-        return ans ;
+        return MaxArea;
     }
 };
