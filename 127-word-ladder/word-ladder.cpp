0e07c4ac3a91e47162f2a9c6392c4b2cf86a2fe0
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-02-18 16:10:41 +0530

    Time: 66 ms (51.73%) | Memory: 21.1 MB (71.03%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList[0].size();
+        int steps  = 0 ;
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        queue<pair<string , int>>que ;
+        que.push({beginWord,steps});
+        while(!que.empty()){
+            string word = que.front().first ;
+            int steps = que.front().second ;
+            string original = word;
+            que.pop();
+            if(word == endWord)return steps+1 ;
+            for(int i = 0 ;i< n ;i++){
+                
+                for(char alpa = 'a' ; alpa<='z' ;alpa++){
+                    word[i] = alpa ;
+                    if(st.find(word)!=st.end()){
+                        que.push({word,steps+1});
+                        st.erase(word);
+                    }      
+                }
+                word = original ;
+            }
+        }
+        return 0 ;
+
+    }
+};
