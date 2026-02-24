bd97e1fa461106edae30b83b6e041158c7bdc33c
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-02-24 14:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (53.22%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -11,36 +11,45 @@
  */
 class Solution {
 public:
-    int binarytonumber(string s){
-        int sum = 0 ;
-        int two = 1 ;
-        for(int i = s.length()-1; i>=0;i--){
-            char c = s[i];
-            int j = c - '0';
-            sum = sum + (two*j) ;
-            two = two*2 ;
-        }
-        return sum ;
+    int chatgpt(TreeNode* root  , int curr){
+        if(root==NULL)return  0 ;
+
+        curr = (curr<<1)|root->val ;
+        if(root->left == NULL && root ->right== NULL )return curr ;
+
+        return chatgpt(root->left, curr) +chatgpt(root->right,curr);
     }
-    int solve(TreeNode* root , string w){
-        if(root==NULL)return 0 ; 
+     // int binarytonumber(string s){//mysol
+    //     int sum = 0 ;
+    //     int two = 1 ;
+    //     for(int i = s.length()-1; i>=0;i--){
+    //         char c = s[i];
+    //         int j = c - '0';
+    //         sum = sum + (two*j) ;
+    //         two = two*2 ;
+    //     }
+    //     return sum ;
+    // }
+    // int solve(TreeNode* root , string w){
+    //     if(root==NULL)return 0 ; 
 
-        w = w + to_string(root->val);
+    //     w = w + to_string(root->val);
 
-        if(root->left == NULL && root->right == NULL ){
-           int ans = binarytonumber(w);
-           return ans;
-        }
+    //     if(root->left == NULL && root->right == NULL ){
+    //        int ans = binarytonumber(w);
+    //        return ans;
+    //     }
         
-        int left  =  solve(root->left,w);
-        int right  = solve(root->right,w) ;
-        return left + right ;
+    //     int left  =  solve(root->left,w);
+    //     int right  = solve(root->right,w) ;
+    //     return left + right ;
 
         
-    }
+    // }
     int sumRootToLeaf(TreeNode* root) {
         if(root == NULL)return 0 ;
-        return solve(root,"") ;
+        // return solve(root,"") ;
+        return chatgpt(root, 0 );
         
     }
 };
