c1c12849375e82f63fd2c9de97bba2e2e697a3f1
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-02-25 09:26:45 +0530

    Time: 7 ms (19.88%) | Memory: 14.8 MB (32.46%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -5,13 +5,8 @@ public:
         int n = arr.size() ;
         for(int i = 0 ;i < n ;i ++){
             int x = arr[i] ;
-            int count = 0 ;
-            
-           while(x != 0){
-            int result= x & 1;
-            if(result)count++ ;
-            x = x >> 1;
-           }
+            int count = __builtin_popcount(x);
+           
            ans.push_back({count,arr[i]});
         }
         sort(ans.begin(),ans.end()) ;
