ec4fa47fcaa956d0705cb352291f2b606b54c857
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-03-09 20:33:35 +0530

    Time: 227 ms (65.04%) | Memory: 8.7 MB (94.31%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public: 
+    int M = 1e9+7 ;
+    int dp[201][201][2] ;
+    int solve(int limit , int ones ,int zeroes ,bool lastOne){
+        if(ones == 0 && zeroes == 0)return 1 ;
+
+        if(dp[ones][zeroes][lastOne] != -1)return dp[ones][zeroes][lastOne] ;
+        int result = 0 ;
+        if(lastOne == true){
+            for(int i = 1 ;i <= min(limit,zeroes); i++){
+                result = (result + solve(limit,ones,zeroes-i,false))%M;
+            }
+        }
+        else {
+            for(int i = 1 ;i <= min(limit,ones); i++){
+                result = (result + solve(limit,ones-i,zeroes,true))%M;
+            }
+        }
+        return dp[ones][zeroes][lastOne] = result ;
+    }
+    int numberOfStableArrays(int zero, int one, int limit) {
+        memset(dp,-1,sizeof(dp));
+        int solvewithOnes = solve(limit,one ,zero,false);
+        int solvewithZeroes = solve(limit , one ,zero ,true) ;
+        return (solvewithOnes + solvewithZeroes)%M ;
+        
+    }
+};
