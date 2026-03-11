fc66465b6f5cc520d1f87cb1174095b06da797aa
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-03-11 10:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (4.02%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     int bitwiseComplement(int n) {
       
-        int number = 1 ;
-        while(number<n){
-            number = (number<<1 )|1 ;
-        }
-        return number ^ n ;
+        // int number = 1 ;
+        // while(number<n){
+        //     number = (number<<1 )|1 ;
+        // }
+        // return number ^ n ;
+        if(n==0)return 1 ;
+        int bits = floor(log2(n))+1;//this technique is used to find the number of bits 
+        int mask = (1 << bits )- 1; 
+        return n^mask;
+
        
     }
 };
