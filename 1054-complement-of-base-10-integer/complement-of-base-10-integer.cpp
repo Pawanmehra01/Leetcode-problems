96935a49a4a2047670da0b3d437bf81efddef07e
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-03-11 10:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (4.02%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -7,7 +7,8 @@ public:
         //     number = (number<<1 )|1 ;
         // }
         // return number ^ n ;
-        if(n==0)return 1 ;
+        if(n==0)return 1 ;// value of log 0 will be  towards - infinity or undefined
+        
         int bits = floor(log2(n))+1;//this technique is used to find the number of bits 
         int mask = (1 << bits )- 1; 
         return n^mask;
