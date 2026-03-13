8fabe90dd4758cc007c11296e2b0ec063da0e9ef
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-03-13 10:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (10.80%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,13 +4,13 @@ public:
         int n = arr.size() ;
         int low = 0 ;
         int high = n-1 ;
-        while(low<high){
+        while(low<=high){
             int mid = low + (high -low )/2 ;
             if(arr[mid]<arr[mid+1]){
                 low = mid + 1 ;
 
             }else{
-                high = mid ;
+                high = mid-1 ;
             }
         }
         return low ;
