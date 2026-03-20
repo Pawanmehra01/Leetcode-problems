2cb8366686d669c16ccd028b48bea9ce336687c9
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-03-20 17:16:09 +0530

    Time: 12 ms (25.79%) | Memory: 84.3 MB (50.14%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -5,20 +5,16 @@ public:
         int j = 0 ;
         int n = nums.size() ;
         int Ones = accumulate(begin(nums),end(nums),0) ;
-        vector<int>temp(2*n);
-        for(int i = 0 ;i < 2*n  ;i ++ ){
-            temp[i] = nums[i%n] ;
-        }
-
         //classic sliding window template 
+        // now with space optimization
         int count_ones = 0 ;
         int min_zero_window  = 0 ;
         while(j<2*n){
-          if(temp[j]){
+          if(nums[j%n]){
             count_ones++ ;
           }
           if(j-i+1 > Ones){
-            count_ones -= temp[i];
+            count_ones -= nums[i%n];
             i++;
           }
         min_zero_window = max(min_zero_window,count_ones) ;
