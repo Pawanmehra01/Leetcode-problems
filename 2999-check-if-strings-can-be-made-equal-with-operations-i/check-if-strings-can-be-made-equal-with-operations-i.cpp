bfb2306c512f81e92d7d066df34e647463f29c49
Pawan singh mehra <Pawan7mehra12@gmail.com>
2026-03-29 06:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (96.13%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        int i = 0 ;
+        int j = 2 ;
+        
+        while(j<4){
+            if(s1==s2)return true;
+
+            if(s1[i] == s2[i]){
+                i++ ;
+                j++ ;
+            }
+            else{
+                swap(s1[i],s1[j]);
+                i++;
+                j++;
+            }
+        }
+        j=2 ;
+        i=0;
+        while(j<4){
+            if(s1==s2)return true;
+
+            if(s1[i] == s2[i]){
+                i++ ;
+                j++ ;
+            }
+            else{
+                swap(s2[i],s2[j]);
+                i++;
+                j++;
+            }
+        }
+        if(s1==s2)return true;
+        return false;
+    }
+};
